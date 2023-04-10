60e1f92287a2f689fad650f198031f1856ae8114
Richard Hounslow <rhounslow@gmail.com>
2023-04-10 15:31:32 +1000

    blah img

--- pages/index.js
+++ pages/index.js
@@ -61,8 +61,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          Powered by{' '} Blah
+          {/* <img src="/vercel.svg" alt="Vercel" className={styles.logo} /> */}
         </a>
       </footer>
 
