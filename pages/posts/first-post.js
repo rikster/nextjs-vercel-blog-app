a497454239d07775c952dc1fa26532a04a550b24
Richard Hounslow <rhounslow@gmail.com>
2023-04-10 15:31:21 +1000

    Support for third party scripts component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,25 @@
 //Nextjs and Link uses client side nav, code splitting and prefetching
 import React from "react"
-import Link from 'next/link';
+import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
+            </Head>
             <h1>First Post</h1>
             <h2>
-                <Link href="/">Back to home</Link>
+                <Link href="/">← Back to home</Link>
             </h2>
         </>
     );
