a881959352ce431f28e7cd0619af344a41014196
Richard Hounslow <rhounslow@gmail.com>
2023-04-10 15:49:28 +1000

    Layout and CSS Modules

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # A Basic Blog in Next.js
+
  Covering the fundamentals of Next.js
+- Routing
+- Link Component
+- Client-Side Nav
+- Assets, Metadata and CSS
+  - Third Party JS
+  - Layout Component
+    - CSS Modules
+  - Global Styles
+
  
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,10 +3,11 @@ import React from "react"
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script';
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
                 <Script
@@ -21,6 +22,6 @@ export default function FirstPost() {
             <h2>
                 <Link href="/">← Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
