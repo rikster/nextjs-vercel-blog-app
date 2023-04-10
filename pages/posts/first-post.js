a2f24191d20652fd860662f66fa6617a059e1860
Richard Hounslow <rhounslow@gmail.com>
2023-04-10 11:19:50 +1000

    use <Link/>
    which uses client side nav, code splitting and prefetching

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Link from 'next/link';
 
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
 }
