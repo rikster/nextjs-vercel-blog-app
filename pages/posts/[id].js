e8368b5924e62a7e14097b606146ffcc76fd26d4
Richard Hounslow <rhounslow@gmail.com>
2023-04-12 15:34:59 +1000

    wrap posts in <article/>

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
@@ -25,15 +26,14 @@ export default function Post({ postData }) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {/* https://date-fns.org/v2.16.1/docs/format */}
-            {/* <Date dateString={postData.date} /> */}
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    {/* <Date dateString={postData.date} /> */}
+                    {postData.date}
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
